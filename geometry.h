13757eb6b85ece48ef13849b9b9080e9c95e7c96
Aflos <L18253728426@163.com>
2026-09-17 20:25:08 +0800

    add transforms and z-buffer rendering

--- geometry.h
+++ geometry.h
@@ -1,6 +1,237 @@
+#pragma once
+#include <iostream>
+#include <cmath>
 
 struct Point
 {
-    int x;
-    int y;
+    float x;
+    float y;
+    float z;
+
+    void print(){
+        std::cout << x << ' ' << y << ' ' << z << std::endl;
+    }
+};
+
+struct Vec3 {
+    float x;
+    float y;
+    float z;
+
+    Vec3 operator+ (const Vec3& p) const{
+        return{
+            x + p.x,
+            y + p.y,
+            z + p.z
+        };
+    }
+
+    Vec3 operator- (const Vec3& p) const{
+        return{
+            x - p.x,
+            y - p.y,
+            z - p.z
+        };
+    }
+
+    Vec3 operator* (const float p) const{
+        return{
+            x * p,
+            y * p,
+            z * p
+        };
+    }
+
+    float dot (const Vec3& p) const{
+        return{
+            x * p.x +
+            y * p.y +
+            z * p.z
+        };
+    }
+
+    Vec3 cross (const Vec3& p) const{
+        return{
+            y * p.z - z * p.y,
+            z * p.x - x * p.z,
+            x * p.y - y * p.x
+        };
+    }
+
+    float length() const {
+        return std::sqrt(
+            x*x + y*y + z*z
+        );
+    }
+
+
+    Vec3 normalize() const {
+        float len = length();
+        if (len == 0.0f) {
+            return {0, 0, 0};
+        }
+        return{
+            x / len,
+            y / len,
+            z / len
+        };
+    }    
+
+};
+
+struct Vec4 {
+    float x;
+    float y;
+    float z;
+    float w;
+
+    void print() const {
+        std::cout << x << ' '
+                << y << ' '
+                << z << ' '
+                << w << std::endl;
+    }
+};
+
+struct Mat4 {
+    float m[4][4];
+
+    static Mat4 identity() {
+        Mat4 result = {};
+        result.m[0][0] = 1.0f;
+        result.m[1][1] = 1.0f;
+        result.m[2][2] = 1.0f;
+        result.m[3][3] = 1.0f;
+        return result;
+    }
+
+    static Mat4 translation(float tx, float ty, float tz) {
+        Mat4 result = Mat4::identity();
+
+        result.m[0][3] = tx;
+        result.m[1][3] = ty;
+        result.m[2][3] = tz;
+
+        return result;
+    }
+
+    static Mat4 scale(float sx, float sy, float sz) {
+        Mat4 result = {};
+
+        result.m[0][0] = sx;
+        result.m[1][1] = sy;
+        result.m[2][2] = sz;
+        result.m[3][3] = 1.0f;
+
+        return result;
+    }
+
+    static Mat4 rotationZ(float angle) {
+        Mat4 result = Mat4::identity();
+
+        float c = std::cos(angle);
+        float s = std::sin(angle);
+
+        result.m[0][0] = c;
+        result.m[0][1] = -s;
+
+        result.m[1][0] = s;
+        result.m[1][1] = c;
+
+        return result;
+    }
+
+    Vec4 operator* (const Vec4& p) const{
+        return{
+            m[0][0] * p.x + m[0][1] * p.y + m[0][2] * p.z + m[0][3] * p.w,
+            m[1][0] * p.x + m[1][1] * p.y + m[1][2] * p.z + m[1][3] * p.w,
+            m[2][0] * p.x + m[2][1] * p.y + m[2][2] * p.z + m[2][3] * p.w,
+            m[3][0] * p.x + m[3][1] * p.y + m[3][2] * p.z + m[3][3] * p.w
+        };
+    }
+
+
+    Mat4 operator*(const Mat4& other) const {
+        Mat4 result = {};
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    result.m[i][j] += m[i][k] * other.m[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    ///////////View Matrix ////////////“从相机看来物体在哪里”
+    static Mat4 lookAt(const Vec3& eye, const Vec3& target, const Vec3& up) {
+        Vec3 forward = (target - eye).normalize();
+        Vec3 right = forward.cross(up).normalize();
+        Vec3 cameraUp = right.cross(forward);
+
+        Mat4 result = Mat4::identity();
+
+        result.m[0][0] = right.x;
+        result.m[0][1] = right.y;
+        result.m[0][2] = right.z;
+        result.m[0][3] = -right.dot(eye);
+
+        result.m[1][0] = cameraUp.x;
+        result.m[1][1] = cameraUp.y;
+        result.m[1][2] = cameraUp.z;
+        result.m[1][3] = -cameraUp.dot(eye);
+
+        result.m[2][0] = -forward.x;
+        result.m[2][1] = -forward.y;
+        result.m[2][2] = -forward.z;
+        result.m[2][3] = forward.dot(eye);
+
+        return result;
+    }
+
+    static Mat4 perspective(float fov, float aspect, float near, float far) {
+        Mat4 result = {};
+
+        float f = 1.0f / std::tan(fov / 2.0f);
+
+        result.m[0][0] = f / aspect;
+        result.m[1][1] = f;
+
+        result.m[2][2] = (far + near) / (near - far);
+        result.m[2][3] = (2.0f * far * near) / (near - far);
+
+        result.m[3][2] = -1.0f;
+
+        return result;
+    }
+
+    static Point viewport(const Vec4& ndc, int width, int height) {
+        return {
+            (ndc.x + 1.0f) * 0.5f * (width - 1),
+            (ndc.y + 1.0f) * 0.5f * (height - 1),
+            ndc.z
+        };
+    }
+
+    static Point transformPoint(const Vec3& position,const Mat4& mvp, int width, int height) {
+        Vec4 p = {
+            position.x,
+            position.y,
+            position.z,
+            1.0f
+        };
+
+        Vec4 clip = mvp * p;
+
+        Vec4 ndc = {
+            clip.x / clip.w,
+            clip.y / clip.w,
+            clip.z / clip.w,
+            1.0f
+        };
+
+        return viewport(ndc,width,height);
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <cmath>
+﻿#include <cmath>
 #include "tgaimage.h"
 #include <utility>
 #include "geometry.h"
@@ -10,6 +10,10 @@ constexpr TGAColor green = {0, 255, 0, 255};
 constexpr TGAColor red = {0, 0, 255, 255};
 constexpr TGAColor blue = {255, 128, 64, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
+
+constexpr int width = 1024;
+constexpr int height = 1024;
+
 
 void line(int x0, int y0, int x1, int y1, TGAImage &framebuffer, TGAColor color)
 {
@@ -43,7 +47,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &framebuffer, TGAColor color)
     }
 }
 
-bool insidetriangle(Point a, Point b, Point c, Point p, TGAColor& color){
+bool insidetriangle(Point a, Point b, Point c, Point p, TGAColor& color, float& z){
     if((b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x) == 0 ){
         return false;
     }
@@ -55,10 +59,7 @@ bool insidetriangle(Point a, Point b, Point c, Point p, TGAColor& color){
     if((u >= 0) && (v >= 0) && ((u + v) <= 1)){
         float alpha = 1 - u - v;
         color = alpha * red + u * green + v * blue;
-        // std::cout << alpha << "alpha " << u << "u " << v << " v" 
-        //     << int(color[0]) << 'blue'
-        //     << int(color[1]) << "green"
-        //     << int(color[2]) << "red" << std::endl;
+        z = alpha * a.z + u * b.z + v * c.z;
         return true;
     }
     else{
@@ -66,20 +67,42 @@ bool insidetriangle(Point a, Point b, Point c, Point p, TGAColor& color){
     }
 }
 
-void triangle(Point a, Point b, Point c, TGAImage& framebuffer, TGAColor color){
-    int minx = std::min({a.x, b.x, c.x});
-    int maxx = std::max({a.x, b.x, c.x});
-    int miny = std::min({a.y, b.y, c.y});
-    int maxy = std::max({a.y, b.y, c.y});
+void triangle(Point a, Point b, Point c, TGAImage& framebuffer, TGAColor color, std::vector<float>& zbuffer){
+    int minx = static_cast<int>(
+        std::floor(std::min({a.x, b.x, c.x}))
+    );
+
+    int maxx = static_cast<int>(
+        std::ceil(std::max({a.x, b.x, c.x}))
+    );
+
+    int miny = static_cast<int>(
+        std::floor(std::min({a.y, b.y, c.y}))
+    );
+
+    int maxy = static_cast<int>(
+        std::ceil(std::max({a.y, b.y, c.y}))
+    );
+
+    minx = std::max(minx, 0);
+    maxx = std::min(maxx, width - 1);
+
+    miny = std::max(miny, 0);
+    maxy = std::min(maxy, height - 1);
 
     Point p;
 
     for(int x = minx; x <= maxx; x++){
         for(int y = miny; y <= maxy; y++){
-            p.x = x;
-            p.y = y;
-            if(insidetriangle(a, b, c, p,color)){
-                framebuffer.set(p.x, p.y, color);
+            p.x = x + 0.5f;
+            p.y = y + 0.5f;
+            float z;
+            if(insidetriangle(a, b, c, p,color, z)){
+                if( z < zbuffer[x + y * width]){
+                    zbuffer[x + y * width] = z;
+                    framebuffer.set(x, y, color);
+                }
+                
             }
         }
     }
@@ -87,9 +110,16 @@ void triangle(Point a, Point b, Point c, TGAImage& framebuffer, TGAColor color){
 
 int main(int argc, char **argv)
 {
-    constexpr int width = 1024;
-    constexpr int height = 1024;
+
     TGAImage framebuffer(width, height, TGAImage::RGB);
+
+    std::vector<float> zbuffer(width * height);
+
+    std::fill(
+        zbuffer.begin(),
+        zbuffer.end(),
+        std::numeric_limits<float>::infinity()
+    );
 
 
     // framebuffer.set(ax, ay, blue);
@@ -111,14 +141,75 @@ int main(int argc, char **argv)
     // line(600, 600, 600, 600, framebuffer, red);     // 水平
     
 
-    Point a = {100, 100};
-    Point b = {800, 200};
-    Point c = {400, 800};
-    line(a.x, a.y, b.x, b.y, framebuffer, red); 
-    line(b.x, b.y, c.x, c.y, framebuffer, red); 
-    line(c.x, c.y, a.x, a.y, framebuffer, red); 
-    triangle(a, b, c, framebuffer, green);
+    // Point a = {100, 100, 0.2f};
+    // Point b = {800, 200, 0.8f};
+    // Point c = {400, 800, 0.5f};
+    // line(a.x, a.y, b.x, b.y, framebuffer, red); 
+    // line(b.x, b.y, c.x, c.y, framebuffer, red); 
+    // line(c.x, c.y, a.x, a.y, framebuffer, red); 
+    // triangle(a, b, c, framebuffer, green, zbuffer);
 
+    // Point d = {500, 100, 0.3f};
+    // Point e = {200, 600, 0.8f};
+    // Point f = {600, 800, 0.5f};
+    // line(d.x, d.y, e.x, e.y, framebuffer, red); 
+    // line(e.x, e.y, f.x, f.y, framebuffer, red); 
+    // line(f.x, f.y, d.x, d.y, framebuffer, red); 
+    // triangle(d, e, f, framebuffer, red, zbuffer);
+
+    // framebuffer.write_tga_file("framebuffer.tga");
+
+    // ///////////////////////////////////////test for S R T
+    // Vec4 p = {1, 0, 0, 1};
+
+    // Mat4 S = Mat4::scale(2, 2, 2);
+    // Mat4 R = Mat4::rotationZ(3.1415926f / 2.0f);
+    // Mat4 T = Mat4::translation(10, 20, 30);
+
+    // Mat4 M = T * R * S;
+
+    // Vec4 result = M * p;
+    // result.print();
+
+    
+    
+    Vec3 A = {-1.0f, -1.0f, 0.0f};
+    Vec3 B = { 1.0f, -1.0f, 0.0f};
+    Vec3 C = { 0.0f,  1.0f, 0.0f};
+////////////Model//////////
+    Mat4 S = Mat4::scale(0.8f, 0.8f, 0.8f);
+    Mat4 R = Mat4::rotationZ(3.1415926f / 4.0f);
+    Mat4 T = Mat4::translation(0.5f, 0.0f, 0.0f);
+
+    Mat4 M = T * R * S;
+
+///////////////View Matrix////////////////////
+    Vec3 eye    = {0, 0, 3};
+    Vec3 target = {0, 0, 0};
+    Vec3 up     = {0, 1, 0};
+
+    Mat4 V = Mat4::lookAt(eye, target, up);
+
+//////////Projection///////////////////////
+    float fov = 90.0f * 3.1415926f / 180.0f;
+
+    Mat4 P = Mat4::perspective(
+        fov,
+        1.0f,
+        0.1f,
+        100.0f
+    );
+
+/////////////////////MVP////////
+
+    Mat4 MVP = P * V * M;
+
+    Point ScrA = Mat4::transformPoint(A, MVP, width, height);
+    Point ScrB = Mat4::transformPoint(B, MVP, width, height);
+    Point ScrC = Mat4::transformPoint(C, MVP, width, height);
+
+    triangle(ScrA, ScrB, ScrC, framebuffer, red, zbuffer);
     framebuffer.write_tga_file("framebuffer.tga");
+
     return 0;
 }
